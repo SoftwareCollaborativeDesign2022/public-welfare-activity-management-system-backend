609700cab1ea2ae23f366f76682d387cf9dffcab
Xufuhao <saikumo11@gmail.com>
2022-04-02 15:14:19 +0800

    项目初始化

---
+++ src/main/java/org/saikumo/pwams/PublicWelfareActivityManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package org.saikumo.pwams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicWelfareActivityManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicWelfareActivityManagementSystemBackendApplication.class, args);
+	}
+
+}
